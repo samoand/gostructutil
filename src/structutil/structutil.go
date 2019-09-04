bb7dee85b36e266ed84461c63791af59626f7c94
Andrew Samoylov <andrew@apstrktr.com>
2019-09-04 10:32:30 -0700

    map merge functionality

---
+++ src/structutil/structutil.go
@@ -0,0 +1,150 @@
+package structutil
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"github.com/golang/glog"
+	"strings"
+)
+
+func MakeMarshalFriendly(in *map[interface{}]interface{}) *map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, value := range *in {
+		keyAsStr := fmt.Sprintf("%v", key)
+		valueAsMap, ok := value.(map[interface{}]interface{})
+		if ok {
+			result[keyAsStr] = *MakeMarshalFriendly(&valueAsMap)
+		} else {
+			result[keyAsStr] = value
+		}
+	}
+
+	return &result
+}
+
+func CloneMap(amap *map[interface{}]interface{},
+	typeSampleValues ...interface{}) *map[interface{}]interface{} {
+	if typeSampleValues != nil {
+		for _, value := range typeSampleValues {
+			gob.Register(value)
+		}
+	}
+	buff := new(bytes.Buffer)
+	enc := gob.NewEncoder(buff)
+	dec := gob.NewDecoder(buff)
+	err := enc.Encode(*amap)
+	if err != nil {
+		panic(err)
+	}
+	res := make(map[interface{}]interface{})
+	err = dec.Decode(&res)
+	if err != nil {
+		panic(err)
+	}
+	return &res
+}
+
+func sliceRepr(in []interface{}, sep string) string {
+	asStr := make([]string, len(in))
+	for i, el := range in {
+		asStr[i] = fmt.Sprintf("%v", el)
+	}
+	return strings.Join(asStr, sep)
+}
+
+func Merge(
+	one *map[interface{}]interface{},
+	two *map[interface{}]interface{},
+	immutable bool,
+	typeSampleValues ...interface{}) (*map[interface{}]interface{}, error) {
+
+	var source *map[interface{}]interface{}
+	var target *map[interface{}]interface{}
+	if immutable {
+		source = CloneMap(two, typeSampleValues...)
+		target = CloneMap(one, typeSampleValues...)
+	} else {
+		source = two
+		target = one
+	}
+	var inner func(
+		source *map[interface{}]interface{},
+		target *map[interface{}]interface{},
+		priorKeys []interface{}) (*map[interface{}]interface{}, error)
+	inner = func(
+		source *map[interface{}]interface{},
+		target *map[interface{}]interface{},
+		priorKeys []interface{}) (*map[interface{}]interface{}, error) {
+
+		for sourceKey, sourceValue := range *source {
+			keysRepr := func() string {
+				priorKeysAsDn := sliceRepr(priorKeys, "/")
+				if len(priorKeysAsDn) > 0 {
+					priorKeysAsDn = "/" + priorKeysAsDn
+				}
+				return priorKeysAsDn + "/" + fmt.Sprintf("%v", sourceKey)
+			}
+			targetValue, existsInTarget := (*target)[sourceKey]
+			if existsInTarget {
+				targetValueAsMap, targetValueIsMap := targetValue.(map[interface{}]interface{})
+				sourceValueAsMap, sourceValueIsMap := sourceValue.(map[interface{}]interface{})
+				if targetValueIsMap != sourceValueIsMap {
+					err := fmt.Sprintf(
+						"different types at key %s detected in trying to merge maps",
+						keysRepr())
+					glog.Error(err)
+					return nil, errors.New(err)
+				} else if !targetValueIsMap {
+					glog.Warning(fmt.Sprintf("Key clash at %s, value %v overrides value %v",
+						keysRepr(), sourceValue, targetValue))
+					(*target)[sourceKey] = sourceValue
+				} else {
+					inner(&sourceValueAsMap, &targetValueAsMap, append(priorKeys, sourceKey))
+				}
+
+			} else {
+				(*target)[sourceKey] = (*source)[sourceKey]
+			}
+		}
+
+		return target, nil
+	}
+	return inner(source, target, make([]interface{}, 0))
+}
+
+func MergeAll(ms []*map[interface{}]interface{}, immutable bool,
+	typeSampleValues ...interface{}) (*map[interface{}]interface{}, error) {
+	type MergeResult = struct {
+		result *map[interface{}]interface{}
+		err    error
+	}
+	switch size := len(ms); size {
+	case 0:
+		return nil, errors.New("mergeall on 0 elements? really?")
+	case 1:
+		return ms[0], nil
+	default:
+		mid := int(size / 2)
+		chOne := make(chan MergeResult)
+		chTwo := make(chan MergeResult)
+		go func() {
+			res, err := MergeAll(ms[0:mid], immutable, typeSampleValues...)
+			chOne <- MergeResult{res, err}
+		}()
+		go func() {
+			res, err := MergeAll(ms[mid:], immutable, typeSampleValues...)
+			chTwo <- MergeResult{res, err}
+		}()
+		one := <-chOne
+		if one.err != nil {
+			return nil, one.err
+		}
+		two := <-chTwo
+		if two.err != nil {
+			return nil, two.err
+		}
+		return Merge(one.result, two.result, immutable, typeSampleValues...)
+	}
+}
---
+++ src/structutil/structutil_test.go
@@ -0,0 +1,221 @@
+package structutil
+
+import (
+	"encoding/json"
+	"gotest.tools/assert"
+	"testing"
+)
+
+var TypeValuesToRegister []interface{}
+
+var one map[interface{}]interface{}
+var two map[interface{}]interface{}
+var three map[interface{}]interface{}
+var four map[interface{}]interface{}
+var five map[interface{}]interface{}
+var six map[interface{}]interface{}
+var mergeResult map[interface{}]interface{}
+var intermixed map[interface{}]interface{}
+var intermixedMergeResult map[interface{}]interface{}
+var intermixedBad map[interface{}]interface{}
+
+func initialize() {
+	TypeValuesToRegister = []interface{}{map[interface{}]interface{}{}, map[string]interface{}{}, map[string]string{}}
+
+	one = map[interface{}]interface{}{
+		"keyOne1": map[interface{}]interface{}{
+			"keyOne1_1": "valueOneOne1_1",
+		},
+		"keyOne2": "valueOne2",
+		"keyOne3": map[interface{}]interface{}{
+			"keyOne3_1": "valueOne3_1",
+			"keyOne3_2": "valueOne3_2",
+			"keyOne3_3": map[interface{}]interface{}{
+				"keyOne4_1": "valueOne4_1",
+			},
+		},
+	}
+	two = map[interface{}]interface{}{
+		"keyTwo1": map[interface{}]interface{}{
+			"keyTwo1_1": "valueTwo1_1",
+		},
+		"keyTwo2": "valueTwo2",
+		"keyTwo3": map[interface{}]interface{}{
+			"keyTwo3_1": "valueTwo3_1",
+			"keyTwo3_2": "valueTwo3_2",
+			"keyTwo3_3": map[interface{}]interface{}{
+				"keyTwo4_1": "valueTwo4_1",
+			},
+		},
+		"keyTwo4": "valueTwo4",
+	}
+	three = map[interface{}]interface{}{
+		"keyThree1": map[interface{}]interface{}{
+			"keyThree1_1": map[interface{}]interface{}{
+				"keyThree1_1_1": "valueThree1_1_1",
+			},
+		},
+		"keyThree2": "valueThree2",
+		"keyThree3": map[interface{}]interface{}{
+			"keyThree3_1": "valueThree3_1",
+			"keyThree3_2": "valueThree3_2",
+			"keyThree3_3": map[interface{}]interface{}{
+				"keyThree4_1": "valueThree4_1",
+			},
+		},
+	}
+	four = map[interface{}]interface{}{
+		"keyFour1": map[interface{}]interface{}{
+			"keyFour1_1": map[interface{}]interface{}{
+				"keyFour1_1_1": "valueFour1_1_1",
+			},
+		},
+	}
+	five = map[interface{}]interface{}{
+		"keyFive1": "valueFive1",
+	}
+	six = map[interface{}]interface{}{
+		"keySix1": "valueSix1",
+	}
+
+	mergeResult = map[interface{}]interface{}{
+		"keyFive1": "valueFive1",
+		"keyFour1": map[interface{}]interface{}{
+			"keyFour1_1": map[interface{}]interface{}{
+				"keyFour1_1_1": "valueFour1_1_1",
+			},
+		},
+		"keyOne1": map[interface{}]interface{}{
+			"keyOne1_1": "valueOneOne1_1",
+		},
+		"keyOne2": "valueOne2",
+		"keyOne3": map[interface{}]interface{}{
+			"keyOne3_1": "valueOne3_1",
+			"keyOne3_2": "valueOne3_2",
+			"keyOne3_3": map[interface{}]interface{}{
+				"keyOne4_1": "valueOne4_1",
+			},
+		},
+		"keySix1": "valueSix1",
+		"keyThree1": map[interface{}]interface{}{
+			"keyThree1_1": map[interface{}]interface{}{
+				"keyThree1_1_1": "valueThree1_1_1",
+			},
+		},
+		"keyThree2": "valueThree2",
+		"keyThree3": map[interface{}]interface{}{
+			"keyThree3_1": "valueThree3_1",
+			"keyThree3_2": "valueThree3_2",
+			"keyThree3_3": map[interface{}]interface{}{
+				"keyThree4_1": "valueThree4_1",
+			},
+		},
+		"keyTwo1": map[interface{}]interface{}{
+			"keyTwo1_1": "valueTwo1_1",
+		},
+		"keyTwo2": "valueTwo2",
+		"keyTwo3": map[interface{}]interface{}{
+			"keyTwo3_1": "valueTwo3_1",
+			"keyTwo3_2": "valueTwo3_2",
+			"keyTwo3_3": map[interface{}]interface{}{
+				"keyTwo4_1": "valueTwo4_1",
+			},
+		},
+		"keyTwo4": "valueTwo4",
+	}
+	intermixed = map[interface{}]interface{}{
+		"keyTwo1": map[interface{}]interface{}{
+			"keyOne1_1": map[interface{}]interface{}{
+				"keyTwo1_1_1": "valueTwo1_1_1",
+			},
+		},
+		"keyTwo3": map[interface{}]interface{}{
+			"keyTwo3_1": "valueTwo3_1_modified",
+		},
+	}
+	intermixedMergeResult = map[interface{}]interface{}{
+		"keyTwo1": map[interface{}]interface{}{
+			"keyOne1_1": map[interface{}]interface{}{
+				"keyTwo1_1_1": "valueTwo1_1_1",
+			},
+			"keyTwo1_1": "valueTwo1_1",
+		},
+		"keyTwo2": "valueTwo2",
+		"keyTwo3": map[interface{}]interface{}{
+			"keyTwo3_1": "valueTwo3_1_modified",
+			"keyTwo3_2": "valueTwo3_2",
+			"keyTwo3_3": map[interface{}]interface{}{
+				"keyTwo4_1": "valueTwo4_1",
+			},
+		},
+		"keyTwo4": "valueTwo4",
+	}
+
+	intermixedBad = map[interface{}]interface{}{
+		"keyTwo1": "valueTwo1",
+	}
+
+}
+
+func merge1(t *testing.T, immutable bool) {
+	initialize()
+	mergeableMaps := []*map[interface{}]interface{}{&one, &two, &three, &four, &five, &six}
+	result, err := MergeAll(mergeableMaps, immutable, TypeValuesToRegister...)
+	if err != nil {
+		t.Fatal("Error reported where should be none")
+	}
+	assert.DeepEqual(t, *result, mergeResult)
+}
+
+func TestMerge1(t *testing.T) {
+	merge1(t, false)
+	merge1(t, true)
+}
+
+func merge2(t *testing.T, immutable bool) {
+	initialize()
+	// mergeableMaps = []*map[interface{}]interface{} {&one, &two, &three, &four, &five, &six, &intermixed}
+	mergeableMaps := []*map[interface{}]interface{}{&two, &intermixed}
+	result, err := MergeAll(mergeableMaps, immutable, TypeValuesToRegister...)
+	if err != nil {
+		t.Fatal("Error reported where should be none")
+	}
+	assert.DeepEqual(t, *result, intermixedMergeResult)
+}
+
+func TestMerge2(t *testing.T) {
+	merge2(t, false)
+	merge2(t, true)
+}
+
+func merge3(t *testing.T, immutable bool) {
+	initialize()
+	// mergeableMaps = []*map[interface{}]interface{} {&one, &two, &three, &four, &five, &six, &intermixed}
+	mergeableMaps := []*map[interface{}]interface{}{&two, &intermixedBad}
+	result, err := MergeAll(mergeableMaps, immutable, TypeValuesToRegister...)
+	if err == nil {
+		resultJsonRepr, _ := json.Marshal(MakeMarshalFriendly(result))
+		t.Fatal("Error should've been reported but wasn't. Got this instead:\n" + string(resultJsonRepr))
+	}
+}
+
+func TestMerge3(t *testing.T) {
+	merge3(t, false)
+	merge3(t, true)
+}
+
+func merge4(t *testing.T, immutable bool) {
+	initialize()
+	// mergeableMaps = []*map[interface{}]interface{} {&one, &two, &three, &four, &five, &six, &intermixed}
+	mergeableMaps := []*map[interface{}]interface{}{&intermixedBad, &two}
+	result, err := MergeAll(mergeableMaps, false, TypeValuesToRegister...)
+	if err == nil {
+		resultJsonRepr, _ := json.Marshal(MakeMarshalFriendly(result))
+		t.Fatal("Error should've been reported but wasn't. Got this instead:\n" + string(resultJsonRepr))
+	}
+}
+
+func TestMerge4(t *testing.T) {
+	merge4(t, false)
+	merge4(t, true)
+}
